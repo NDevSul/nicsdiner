4448e6a398f828f2e93bfed95198498049d42231
itrinia <isantoso@student.ciputra.ac.id>
2022-12-21 16:52:00 +0700

    showcart

--- resources/views/showcart.blade.php
+++ resources/views/showcart.blade.php
@@ -70,25 +70,30 @@
                                 </a>
                             </li>
 
-                            <li>
+                            <li class="hide">
                                 @if (Route::has('login'))
-                                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                                        @auth
-                                <li>
+                                    <div>
+                                        @auth </div>
+                                <li class="">
                                     <x-app-layout>
-
                                     </x-app-layout>
-                                <li>
-                                @else
-                                <li> <a href="{{ route('login') }}"
-                                        class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
+                                </li>
+                            @else
+                                <li class="submenu">
+                                    <a href="javascript:;">Profile</a>
+                                    <ul>
+                                        <li> <a href="{{ route('login') }}"
+                                                class="text-sm text-gray-700 dark:text-gray-500 underline">Login</a></li>
 
-                                @if (Route::has('register'))
-                                    <li> <a href="{{ route('register') }}"
-                                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                                    </li>
+                                        @if (Route::has('register'))
+                                            <li> <a href="{{ route('register') }}"
+                                                    class="text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                            </li>
+                                    </ul>
+                                </li>
                                 @endif
                             @endauth
+                            </li>
                         </ul>
                 </div>
                 @endif
@@ -105,67 +110,74 @@
         <div class="px-auto center">
             <table align="center" bgcolor="#FFF9B0" class="border-collapse border border-black">
                 <tr>
+                    <th style="padding: 30px" class="border-collapse border border-black">Action</th>
                     <th style="padding: 30px" class="border-collapse border border-black">Food Name</th>
                     <th style="padding: 30px" class="border-collapse border border-black">Food Price</th>
                     <th style="padding: 30px" class="border-collapse border border-black">Quantity</th>
-                    <th style="padding: 30px" class="border-collapse border border-black">Action</th>
+
                 </tr>
+                @foreach ($cartdata as $cartdata)
+                    <tr style="position: relative; top: -80px; left: auto;">
+                        <td class=""><a href="{{ url('/remove', $cartdata->id) }}"
+                                class="btn btn-warning">Remove</a></td>
+                    </tr>
+                @endforeach
 
                 <form action="{{ url('/orderconfirm') }}" method="POST">
                     @csrf
 
                     @foreach ($data as $data)
                         <tr align="center">
-                            <td class="border-collapse border border-black"><input type="text" name="foodname[]" value="{{ $data->title }}" hidden="">
+                            <td class="border-collapse border border-black py-2"><input type="text" name="foodname[]"
+                                    value="{{ $data->title }}" hidden="">
                                 {{ $data->title }}
                             </td>
-                            <td class="border-collapse border border-black"><input type="text" name="price[]" value="{{ $data->price }}" hidden="">
+                            <td class="border-collapse border border-black"><input type="text" name="price[]"
+                                    value="{{ $data->price }}" hidden="">
                                 {{ $data->price }}
                             </td>
-                            <td class="border-collapse border border-black"><input type="text" name="quantity[]" value="{{ $data->quantity }}" hidden="">
+                            <td class="border-collapse border border-black"><input type="text" name="quantity[]"
+                                    value="{{ $data->quantity }}" hidden="">
                                 {{ $data->quantity }}
                             </td>
                         </tr>
                     @endforeach
+                </form>
 
 
-                    @foreach ($cartdata as $cartdata)
-                        <tr style="position: relative; top: -60px; right: -442px;">
-                            <td><a href="{{ url('/remove', $cartdata->id) }}" class="btn btn-warning">Remove</a></td>
-                        </tr>
-                    @endforeach
 
             </table>
-        </div>
-        <br><br>
 
-        <div class="px-auto center" style="background-color:#FFF9B0">
-            <div class="col-lg-6">
+        </div>
+        <br><br><br>
+
+        <div class="center text-center py-8" style="background-color:#FFF9B0">
+            <div class="center col-lg-6 lg:max-xl:flex">
                 <div class="contact-form">
                     <form id="contact" action="" method="post">
                         <div class="row">
                             <div class="col-lg-12">
-                                <h4 class="font-bold text-center">Order Now</h4>
+                                <h4 class="font-bold text-center pb-4">Order Now</h4>
                             </div>
-                            <div class="col-lg-6 col-sm-12">
+                            <div class="col-lg-12 col-sm-6">
                                 <fieldset>
                                     <input name="name" type="text" id="name" placeholder="Your Name*"
                                         required="">
                                 </fieldset>
                             </div>
-                            <div class="col-lg-6 col-sm-12">
+                            <div class="col-lg-12 col-sm-6">
                                 <fieldset>
                                     <input name="address" type="text" id="address" placeholder="Your Address*"
                                         required="">
                                 </fieldset>
                             </div>
-                            <div class="col-lg-6 col-sm-12">
+                            <div class="col-lg-12 col-sm-6">
                                 <fieldset>
                                     <input name="phone" type="text" id="phone" placeholder="WA Number*"
                                         required="">
                                 </fieldset>
                             </div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-12">
                                 <div id="filterDate2">
                                     <div class="input-group date" data-date-format="dd/mm/yyyy">
                                         <input name="date" id="date" type="text" class="form-control"
@@ -176,20 +188,22 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-12">
+                            <div class="col-lg-12 pt-3">
                                 <fieldset>
                                     <textarea name="notes" rows="6" id="notes" placeholder="Notes"></textarea>
                                 </fieldset>
                             </div>
-                            <div style="padding: 10px">
-                                <input class="btn btn-success" type="submit" value="Check Out">
-
-                                <button id="close" type="button" class="btn btn-danger">Cancel</button>
+                            <div class="col-sm px-10 mt-5">
+                                <button type="submit"class="btn btn-success">Check Out</button>
+                            </div>
+                            <div class="col-sm px-10 mt-5">
+                                <button type="submit" class="btn btn-danger">Cancel</button>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
+
         </div>
 
         </form>
